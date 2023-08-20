9cb496a100c71117cab37d00695af6eb309c4fc2
Manolo <manuelgmaroto@gmail.com>
2023-08-20 18:23:31 +0200

    add file token gitignore

--- src/services/accessapi.js
+++ src/services/accessapi.js
@@ -1,12 +1,13 @@
+import { token } from './token';
 
 const BASE_URL = 'https://api.spotify.com/v1/search?type=track&q=';
-const TOKEN = 'BQDV6wY-XGI3dzUvGJGPScHgY9aNKyPAd7NewUdQ8vYYOxHfocoxSgrPxudn9bp0544_HRh0MNiyHod2r6IwvxiCOtjM05v6FnKRCUZpCy9WBe1g0gQ';
+
 export const fetchSongs = async (search) => {
     try {
         const response = await fetch(`${BASE_URL}${search}`, {
             method: "GET",
             headers: {
-                Authorization: `Bearer ${TOKEN}`
+                Authorization: `Bearer ${token}`
             }
         });
         const data = await response.json();
@@ -15,7 +16,3 @@ export const fetchSongs = async (search) => {
         throw error;
     }
 }
-
-/* curl --request GET \
-  --url 'https://api.spotify.com/v1/search?q=remaster%2520track%3ADoxy%2520artist%3AMiles%2520Davis&type=track' \
-  --header 'Authorization: Bearer BQAKpOnETzj0c3TA_hqmPN7f0EtfrNjawsC0cgBpR1JYagvfxz9Ju7eTeQzaIdMBaZFmMc6U4uJtiZbYEpwd0Nr2WxwFwk2vZj-RHn4YV8QE_FW4x38' */
